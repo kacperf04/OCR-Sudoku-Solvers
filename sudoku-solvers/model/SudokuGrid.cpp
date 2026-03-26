6403919ac00e454151d8e54d1cff28933445c7df
kacperfilip <kacperf.dev@gmail.com>
2026-03-26 20:01:22 +0100

    Feat: Implemented basic operators for SudokuGrid

--- sudoku-solvers/model/SudokuGrid.cpp
+++ sudoku-solvers/model/SudokuGrid.cpp
@@ -3,3 +3,57 @@
 //
 
 #include "SudokuGrid.h"
+
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+SudokuGrid::SudokuGrid(const std::vector<std::vector<int> > &grid) {
+    if (grid.empty()) { throw invalid_argument("The grid cannot be empty."); }
+
+    _n = grid.size();
+    _block_size = static_cast<size_t>(sqrt(_n));
+    if (_block_size * _block_size != _n) { throw invalid_argument("Grid dimensions must be a perfect square."); }
+
+    _grid.reserve(_n * _n);
+    for (size_t i = 0; i < _n; ++i) {
+        if (grid[i].size() != _n) { throw invalid_argument("Row length mismatch."); }
+        _grid.insert(_grid.end(), grid[i].begin(), grid[i].end());
+    }
+}
+
+ostream& operator<< (ostream& os, const SudokuGrid& grid) {
+    size_t horizontalSeparatorLength = grid._n + grid._block_size + grid._block_size * (grid._block_size - 1) + 1;
+    string horizontalSeparator = string(horizontalSeparatorLength, '-');
+
+    os << horizontalSeparator << endl;
+    for (size_t i = 0; i < grid._n; ++i) {
+        for (size_t j = 0; j < grid._n; ++j) {
+            if (j % grid._block_size == 0) {
+                os << '|';
+            } else {
+                os << " ";
+            }
+            os << grid._grid[i*grid._n + j];
+        }
+        os << '|' << endl;
+        if (i % grid._block_size == (grid._block_size - 1)) os << horizontalSeparator << endl;
+    }
+
+    return os;
+}
+
+int& SudokuGrid::operator()(size_t n, size_t m) {
+    if (n >= _n || m >= _n) { throw out_of_range("Index out of range."); }
+
+    return _grid[n*_n + m];
+}
+
+int SudokuGrid::operator()(size_t n, size_t m) const {
+    if (n >= _n || m >= _n) { throw out_of_range("Index out of range."); }
+
+    return _grid[n*_n + m];
+}
+
--- sudoku-solvers/model/SudokuGrid.h
+++ sudoku-solvers/model/SudokuGrid.h
@@ -4,9 +4,20 @@
 
 #ifndef SUDOKU_SOLVERS_SUDOKUGRID_H
 #define SUDOKU_SOLVERS_SUDOKUGRID_H
-
+#include<iostream>
+#include<vector>
 
 class SudokuGrid {
+    size_t _n;
+    size_t _block_size;
+    std::vector<int> _grid;
+
+public:
+    explicit SudokuGrid(const std::vector<std::vector<int>>& grid);
+
+    friend std::ostream& operator<< (std::ostream& os, const SudokuGrid& grid);
+    int& operator() (size_t n, size_t m);
+    int operator() (size_t n, size_t m) const;
 };
 
 

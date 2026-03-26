678557b628322f276e8e7adab2ca0239e7ca84de
kacperfilip <kacperf.dev@gmail.com>
2026-03-26 19:59:29 +0100

    Implemented basic operators for SudokuGrid

---
+++ sudoku-solvers/model/SudokuGrid.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kacperfilip on 3/26/26.
+//
+
+#include "SudokuGrid.h"
---
+++ sudoku-solvers/model/SudokuGrid.h
@@ -0,0 +1,13 @@
+//
+// Created by kacperfilip on 3/26/26.
+//
+
+#ifndef SUDOKU_SOLVERS_SUDOKUGRID_H
+#define SUDOKU_SOLVERS_SUDOKUGRID_H
+
+
+class SudokuGrid {
+};
+
+
+#endif //SUDOKU_SOLVERS_SUDOKUGRID_H
